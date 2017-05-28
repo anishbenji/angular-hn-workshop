38e1cc8ba5cbc09b4216765a2b4bf52f9cfcb31b
Anish Benjamin <anishbenji@gmail.com>
2017-05-28 16:53:33 +0530

    step 10: use HackernewsApi in feed component

--- src/app/feeds/feed/feed.component.ts
+++ src/app/feeds/feed/feed.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HackernewsApiService } from '../../shared/services/services.module';
+
 @Component({
   selector: 'hn-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
-  items: number[];
+  items: any;
 
-  constructor() {
-    this.items = Array(30);
-  }
+  constructor(
+    private _hnApi: HackernewsApiService
+  ) { }
 
   ngOnInit() {
+    this._hnApi.fetchFeed('news', 1)
+      .subscribe(
+        (items: any) => this.items = items,
+        error => console.log('Error fetching feed', error)
+      );
   }
 
 }
