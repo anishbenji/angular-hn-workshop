63cba00d230121b3d73b8c678ed331cb65a9c1ad
Anish Benjamin <anishbenji@gmail.com>
2017-05-28 17:45:31 +0530

    step 19: fetch item details in HackernewsApi service

--- src/app/shared/services/hackernews-api.service.ts
+++ src/app/shared/services/hackernews-api.service.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { NewsItem } from '../models/news-item';
+import { ItemDetail } from '../models/item-detail';
+import { PollResult } from '../models/poll-result';
 
 @Injectable()
 export class HackernewsApiService {
@@ -17,6 +19,32 @@ export class HackernewsApiService {
   fetchFeed(feedType: string, page: number): Observable<NewsItem[]> {
     return this._http
       .get(`${this.baseUrl}/${feedType}?page=${page}`)
+      .map(response => response.json());
+  }
+
+  fetchItemContent(id: number): Observable<ItemDetail> {
+    return this._http
+      .get(`${this.baseUrl}/item/${id}`)
+      .map(response => {
+        const item: ItemDetail = response.json();
+        if (item.type === 'poll') {
+          const pollLength = item.poll.length;
+          item.poll_votes_count = 0;
+          for (let i = 0; i < pollLength; i++) {
+            this.fetchPollContent(item.id + (i + 1))
+              .subscribe((pollResult: PollResult) => {
+                item.poll[i] = pollResult;
+                item.poll_votes_count += pollResult.points;
+              });
+          }
+        }
+        return item;
+      });
+  }
+
+  fetchPollContent(id: number): Observable<PollResult> {
+    return this._http
+      .get(`${this.baseUrl}/item/${id}`)
       .map(response => response.json());
   }
 
